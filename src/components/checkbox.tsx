90b80acfc15e108ff4151f2650e447e256208cc6
lucasrbordigon <lucas.r.bordignon@gmail.com>
2022-10-16 11:43:13 -0300

    ci: Add resolved falled rollup storybook workflow

--- src/components/checkbox.tsx
+++ src/components/checkbox.tsx
@@ -1,14 +1,14 @@
-import * as CheckboxPrimitive from '@radix-ui/react-checkbox';
+import * as checkbox from '@radix-ui/react-checkbox';
 import { Check } from 'phosphor-react'
 
-export interface CheckboxProps extends CheckboxPrimitive.CheckboxProps {}
+export interface CheckboxProps extends checkbox.CheckboxProps {}
 
 export function Checkbox(props: CheckboxProps) {
   return (
-    <CheckboxPrimitive.Root className="w-6 h-6 p-[2px] bg-gray-800 rounded" {...props}>
-      <CheckboxPrimitive.Indicator asChild>
+    <checkbox.Root className="w-6 h-6 p-[2px] bg-gray-800 rounded" {...props}>
+      <checkbox.Indicator asChild>
         <Check weight="bold" className="h-5 w-5 text-cyan-500" />
-      </CheckboxPrimitive.Indicator>
-    </CheckboxPrimitive.Root>
+      </checkbox.Indicator>
+    </checkbox.Root>
   )
 }
